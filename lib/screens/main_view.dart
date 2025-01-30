7934d63b479233662ae3fd8c627fec994c7a2d09
Begüm Yurttabir <149105158+bgmyurttabir@users.noreply.github.com>
2025-01-30 14:34:41 +0300

    Update main_view.dart

--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -11,66 +11,67 @@ class MainView extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            width: 900,
-            height: 780,
-            decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(65),
-                    bottomRight: Radius.circular(65))),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 200,
-                  width: 200,
-                  child: CircleAvatar(
-                    foregroundImage: AssetImage("assets/images/logo.png"),
+          Expanded(
+            child: Container(
+            
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Colors.black),
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(65),
+                      bottomRight: Radius.circular(65))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: CircleAvatar(
+                      foregroundImage: AssetImage("assets/images/logo.png"),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Center(
-                  child: Text(
-                    "CELESTIA BOUTIQUE",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w600),
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
-              ],
+                  Center(
+                    child: Text(
+                      "CELESTIA BOUTIQUE",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
-            width: 700,
-            height: 222,
-            decoration: BoxDecoration(color: Colors.white),
+
+            decoration: const BoxDecoration(color: Colors.white),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 50,
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      fixedSize: Size(400, 55),
+                      fixedSize: const Size(400, 55),
                       backgroundColor: Colors.black,
                       foregroundColor: Colors.grey),
                   onPressed: () {
                    router.goShopView(context);
                   },
-                  child: Text(
+                  child: const Text(
                     "Haydi Başlayalım!",
                     style: TextStyle(fontSize: 19),
                   ),
                 ),
               ],
             ),
-          )
+          ),
+          const SizedBox(height: 10,),
         ],
       ),
     );
